e22b38b7f32b8fe10fd9dfd1d9fbfa9558e553e4
Jaman-34 <Jaman3450000@outlook.com>
2024-07-11 03:03:22 +0000

    v2

---
+++ main.js
@@ -0,0 +1,9 @@
+function openDirect() {
+  document.getElementById("direct").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeDirect() {
+  document.getElementById("direct").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}
